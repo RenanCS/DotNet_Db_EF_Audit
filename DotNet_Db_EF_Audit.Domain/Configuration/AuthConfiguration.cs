2b45d492fb9a6c427f1f957122dcffe78de950ee
Renan Carvalho de Souza <renancarvalhodesouza@gmail.com>
2025-08-20 12:10:00 -0300

    feat: Add configuration authorization

---
+++ DotNet_Db_EF_Audit.Domain/Configuration/AuthConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNet_Db_EF_Audit.Domain.Configuration
+{
+    public record AuthConfiguration
+    {
+        public required string Key { get; init; }
+        public required string Issuer { get; init; }
+        public required string Audience { get; init; }
+    }
+}
